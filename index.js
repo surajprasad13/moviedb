d82074d170da1ea62d359a5a1d7e1b3cdb84bd07
surajprasad13 <surajvishwakarma830@gmail.com>
2020-05-28 20:39:21 +0530

    commit

--- index.js
+++ index.js
@@ -1,128 +1,77 @@
-$(document).ready(function() {
+const apikey = "f322b9b92569cc1303fde9c344d90d40";
+const search =
+  "https://api.themoviedb.org/3/search/movie?api_key=f322b9b92569cc1303fde9c344d90d40&query=";
+const poster = "https://image.tmdb.org/t/p/w200";
+const trending =
+  "https://api.themoviedb.org/3/trending/all/week?api_key=f322b9b92569cc1303fde9c344d90d40";
+const upcoming =
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=f322b9b92569cc1303fde9c344d90d40&language=en-US&page=1";
+const toprated =
+  "https://api.themoviedb.org/3/movie/top_rated?api_key=f322b9b92569cc1303fde9c344d90d40&language=en-US&page=1";
+const detail =
+  "https://api.themoviedb.org/3/movie/640344?api_key=f322b9b92569cc1303fde9c344d90d40&language=en-US";
+//trending values
 
-    const apikey = "f322b9b92569cc1303fde9c344d90d40";
-    const search = "https://api.themoviedb.org/3/search/movie?api_key=f322b9b92569cc1303fde9c344d90d40&query=";
-    const poster = "https://image.tmdb.org/t/p/w200";
-    const trending = "https://api.themoviedb.org/3/trending/all/week?api_key=f322b9b92569cc1303fde9c344d90d40";
-    const upcoming = "https://api.themoviedb.org/3/movie/upcoming?api_key=f322b9b92569cc1303fde9c344d90d40&language=en-US&page=1";
-    const toprated = "https://api.themoviedb.org/3/movie/top_rated?api_key=f322b9b92569cc1303fde9c344d90d40&language=en-US&page=1";
-    const detail = "https://api.themoviedb.org/3/movie/640344?api_key=f322b9b92569cc1303fde9c344d90d40&language=en-US";
-    //trending values
+const movieList = document.getElementById("list");
 
-    function movieSection(movies) {
-        return movies.map((movie) => {
-            if (movie.poster_path) {
-                return `<img src=${poster+movie.poster_path} data-movie-id=${movie.id} href=${movie.id}>`
-            }
-        })
-    }
+const fetchUrl = async () => {
+  const url =
+    "https://api.themoviedb.org/3/trending/all/week?api_key=f322b9b92569cc1303fde9c344d90d40";
 
+  const res = await fetch(url);
+  const data = await res.json();
+  const movie = data.results;
+  console.log(data);
+  displayMovie(movie);
+};
 
-    function createMovieContainer(movies) {
-        var movieElement = document.createElement('div');
-        movieElement.setAttribute('class', 'movie')
-        movieElement.innerHTML = '';
-        const movieTemplate = `
-        <section classs="section">
-        ${movieSection(movies)}
-        </section>
-        <div class="content">
-        <p id="content-close">X</p>
-        </div>`;
-        movieElement.innerHTML = movieTemplate;
-        return movieElement;
-    }
+const displayMovie = (movie) => {
+  const html = movie
+    .map(
+      (result) => `<li onclick="selectMovie(${result.id})">
 
-    function generateUrl(path) {
-        const url = `https://api.themoviedb.org/3${path}?api_key=f322b9b92569cc1303fde9c344d90d40&language=en-US`;
-        return url;
-    }
+  <img src="https://image.tmdb.org/t/p/w200${result.poster_path}" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Rating:${result.vote_average}</b></h4>
+    <p>Release Date:${result.release_date}</p>
+  </div>
+</li>`
+    )
+    .join("");
+  movieList.innerHTML = html;
+};
 
+const selectMovie = async (id) => {
+  const detail = `https://api.themoviedb.org/3/movie/${id}?api_key=f322b9b92569cc1303fde9c344d90d40&language=en-US`;
+  const res = await fetch(detail);
+  const data = await res.json();
+  displayPopup(data);
+};
 
-    $.ajax({
-        method: 'GET',
-        url: trending
-    }).done(function(data) {
-        const movies = data.results;
-        const movieBlock = createMovieContainer(movies);
-        $('.trending').html(movieBlock);
+const displayPopup = (data) => {
+  const html = `<div class="popup">
+  <div class="modal-content">
+  <div class="modal-header">
+    <span class="close" onclick="closePopup()">&times;</span>
+    <h2>Modal Header</h2>
+  </div>
+  <div class="modal-body">
+    <img src="https://image.tmdb.org/t/p/w200${data.backdrop_path}"/>
+    <p>Some other text...${data.id}</p>
+  </div>
+  <div class="modal-footer">
+    <h3>Modal Footer</h3>
+  </div>
+</div></div>
+  `;
 
-    })
+movieList.innerHTML=html+movieList.innerHTML
 
+};
 
-    $("form").submit(function(event) {
-        event.preventDefault();
-        const value = $(".search").val();
-        const path = `/search/movie`;
-        $.ajax({
-            method: 'GET',
-            url: generateUrl(path) + "&query=" + value
-        }).done(function(data) {
-            const movies = data.results;
-            const movieBlock = createMovieContainer(movies);
-            $('.searchresult').html(movieBlock);
+const closePopup = () => {
+  const popup = document.querySelector(".popup");
+  popup.parentElement.removeChild(popup);
+};
 
-
-        })
-
-    })
-
-    $.ajax({
-        method: 'GET',
-        url: upcoming
-    }).done(function(data) {
-        const movies = data.results;
-        const movieBlock = createMovieContainer(movies);
-        $('.upcoming').html(movieBlock);
-
-    })
-
-
-    $.ajax({
-        method: 'GET',
-        url: toprated
-    }).done(function(data) {
-        console.log(data)
-        const movies = data.results;
-        const movieBlock = createMovieContainer(movies);
-        $('.toprated').html(movieBlock);
-
-    })
-
-    $('div').click(function(event) {
-
-        const target = event.target;
-        if (target.tagName.toLowerCase = 'img') {
-
-            const section = event.target.parentElement;
-            const content = section.nextElementSibling;
-            content.classList.add('content-display')
-        }
-
-        if (target.id === 'content-close') {
-            const content = event.target.parentElement;
-            content.classList.remove('content-display')
-        }
-
-        const id = target.dataset.movieId;
-        console.log(id);
-        const path = `/movie/${id}`;
-        $.ajax({
-            method: 'GET',
-            url: generateUrl(path)
-        }).done(function(data) {
-            const movies = data.results;
-            const movieBlock = createMovieContainer(movies);
-            $('.content').html(movieBlock);
-            console.log();
-
-        })
-
-    })
-
-
-
-
-
-
-})
+fetchUrl();
